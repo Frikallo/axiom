65006a45ed962e0850a14fc322b2f51cf662c19c
Noah Kay <noahskay@icloud.com>
2026-02-08 17:02:52 -0800

    Add Sphinx docs and Read the Docs config

---
+++ docs/conf.py
@@ -0,0 +1,17 @@
+project = "Axiom"
+author = "Axiom Contributors"
+
+extensions = [
+    "myst_parser",
+]
+
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".md": "markdown",
+}
+
+html_theme = "pydata_sphinx_theme"
+
+html_theme_options = {
+    "github_url": "https://github.com/Frikallo/axiom",
+}
